430a081d2e1e572f7feddc1c9402fcf85aff2838
Kayo Kennedy da Silva <20221230050@ad.ifpb.edu.br>
2024-10-22 10:55:41 -0300

    dataehora

---
+++ questões/dataehora.js
@@ -0,0 +1,5 @@
+
+// 1) Escreva um programa em Javascript que exiba propriedades de data e hora no seguinte
+// formato:
+// ◦ Hoje é: [dia da semana].
+// ◦ A hora atual é: [hora atual no formato de HH:MM:SS]
